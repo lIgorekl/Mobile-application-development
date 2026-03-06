657db8965ff29fcd4c6dfe38407134166737480f
Igor <Ikorovkin11@gmail.com>
2026-03-06 21:46:10 +0300

    practice_1

---
+++ Practice_1/Practice/buttonclicker/src/main/java/ru/mirea/korovkiniv/buttonclicker/MainActivity.java
@@ -0,0 +1,36 @@
+package ru.mirea.korovkiniv.buttonclicker;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onItIsNotMeClick(View view) {
+
+        TextView tv = findViewById(R.id.tvOut);
+        CheckBox checkBox = findViewById(R.id.checkBox);
+
+        tv.setText("It is not me");
+        checkBox.setChecked(false);
+    }
+}
